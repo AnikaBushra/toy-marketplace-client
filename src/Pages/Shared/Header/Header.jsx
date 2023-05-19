bb00f9c8805ad66ae36433fc5b3679b2cfc6d8d8
AnikaBushra <anikabushra024@gmail.com>
2023-05-19 08:41:52 +0600

    user title added

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const user = "anidk"
+    const user = { name: "anika" }
+
     const navList = <>
         <li><Link to='/'></Link></li>
         <li><Link to='/allToys'>All Toys</Link></li>
@@ -44,7 +45,7 @@ const Header = () => {
                 <ul className="flex">
                     {
                         user ? <><li className="my-auto">Log Out</li>
-                            <li className="w-14 mx-3 rounded-full ring "> <img className=" p-2 " src="https://img.freepik.com/free-vector/kids-zone-fun-play-banner-design_1017-33753.jpg?w=740&t=st=1684429807~exp=1684430407~hmac=8db30de4bfe90822aca583c4c456f54d2a39c042f4fa55f7034bfd9730a2064f" /></li></>
+                            <li className="w-14 mx-3 rounded-full ring "> <img title={user.name} className=" p-2 " src="https://img.freepik.com/free-vector/kids-zone-fun-play-banner-design_1017-33753.jpg?w=740&t=st=1684429807~exp=1684430407~hmac=8db30de4bfe90822aca583c4c456f54d2a39c042f4fa55f7034bfd9730a2064f" /></li></>
                             :
                             <li> <Link to='/login'>Log In</Link></li>
                     }
